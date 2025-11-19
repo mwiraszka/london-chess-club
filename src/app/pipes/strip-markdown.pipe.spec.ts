f19af0a58e3080eaacc7298cf65490fa726e5862
mwiraszka <michal@wiraszka.com>
2025-11-19 06:42:01 -0500

    fix: Bust image cache with each new app version release and remove some potential race conditions when determining what image data to fetch and when; Ensure new special embedded image syntax is hidden from article previews

--- src/app/pipes/strip-markdown.pipe.spec.ts
+++ src/app/pipes/strip-markdown.pipe.spec.ts
@@ -20,4 +20,30 @@ describe('StripMarkdownPipe', () => {
 
     expect(pipe.transform('&#39;text&#39; abc |--text|--')).toBe("'text' abc text");
   });
+
+  it('removes special image syntax', () => {
+    expect(pipe.transform('{{{https://example.com/image.jpg}}}')).toBe('');
+    expect(pipe.transform('{{{image-id}}}')).toBe('');
+    expect(pipe.transform('{{{https://example.com/image.jpg}}}(((300)))')).toBe('');
+    expect(pipe.transform('{{{https://example.com/image.jpg}}}<<<caption>>>')).toBe('');
+    expect(
+      pipe.transform('{{{https://example.com/image.jpg}}}(((300)))<<<caption>>>'),
+    ).toBe('');
+
+    expect(
+      pipe.transform(
+        'text before {{{https://example.com/image.jpg}}}(((300)))<<<caption>>> text after',
+      ),
+    ).toBe('text before  text after');
+
+    expect(
+      pipe.transform('abc {{{url}}}(((300)))<<<caption with > character>>> def'),
+    ).toBe('abc  def');
+
+    expect(
+      pipe.transform(
+        'first {{{url1}}}(((100)))<<<caption1>>> middle {{{url2}}}<<<caption2>>> last',
+      ),
+    ).toBe('first  middle  last');
+  });
 });
