0039bcc9d772b9a56519aa9ec91412d36b6c7f3a
mwiraszka <michal@wiraszka.com>
2026-04-16 00:11:28 -0400

    chore: clean up

--- src/app/components/photo-carousel/photo-carousel.component.ts
+++ src/app/components/photo-carousel/photo-carousel.component.ts
@@ -8,6 +8,7 @@ import {
   Component,
   ElementRef,
   HostListener,
+  inject,
   Input,
   OnInit,
 } from '@angular/core';
@@ -27,12 +28,9 @@ export class PhotoCarouselComponent implements OnInit {
 
   public currentIndex = 0;
 
+  private readonly changeDetectorRef = inject(ChangeDetectorRef);
+  private readonly elementRef = inject(ElementRef<HTMLElement>);
   private readonly autoCycleSubject$ = new Subject<void>();
-
-  constructor(
-    private readonly changeDetectorRef: ChangeDetectorRef,
-    private readonly elementRef: ElementRef<HTMLElement>,
-  ) {}
 
   public ngOnInit(): void {
     this.autoCycleSubject$
@@ -51,7 +49,6 @@ export class PhotoCarouselComponent implements OnInit {
   public onPreviousPhoto(): void {
     this.currentIndex = (this.currentIndex - 1 + this.photos.length) % this.photos.length;
     this.autoCycleSubject$.next();
-    this.changeDetectorRef.markForCheck();
   }
 
   @HostListener('keydown.arrowright')
@@ -59,13 +56,10 @@ export class PhotoCarouselComponent implements OnInit {
   public onNextPhoto(): void {
     this.currentIndex = (this.currentIndex + 1) % this.photos.length;
     this.autoCycleSubject$.next();
-    this.changeDetectorRef.markForCheck();
-    this.elementRef.nativeElement.focus();
   }
 
   public onSelectPhoto(index: number): void {
     this.currentIndex = index;
     this.autoCycleSubject$.next();
-    this.elementRef.nativeElement.focus();
   }
 }
