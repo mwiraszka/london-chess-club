f0c4d750599887f429c07659b2c2f8ba05895ff8
mwiraszka <michal@wiraszka.com>
2026-05-03 02:21:16 +0200

    chore: configure Sentry DSN for prod and preview

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   clerkPublishableKey: 'pk_live_Y2xlcmsubG9uZG9uY2hlc3MuY2Ek',
   googleMapsApiKey: 'AIzaSyCeuTgq4qe2k8obnbYWrwWsG91B6aDUSA0',
   lccApiBaseUrl: 'https://api.londonchess.ca/v1',
-  sentryDsn: '',
+  sentryDsn:
+    'https://e5cadc144366ae7d855531a5fad86ac0@o4511323014234112.ingest.de.sentry.io/4511323081539664',
 };
